da821cb57a86f3cfad5afaf850cfb5bd8e446507
antonpavlov <a_pavlov@bingo-boom.com>
2017-03-09 23:58:24 +0300

    add

--- app/src/main/java/ru/barmaglot/android6/finance/fragments/SprListFragment.java
+++ app/src/main/java/ru/barmaglot/android6/finance/fragments/SprListFragment.java
@@ -94,6 +94,7 @@ public class SprListFragment extends Fragment {
         mListener = null;
     }
 
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
